ac9bf8bcbb2f4f07d06bfef3c1a530555b4a021a
Marek Szczepanski <mszczepanski2@atlassian.com>
2021-04-17 12:53:16 +0200

    * Location extended

--- src/main/java/pl/sda/hibernate/entity/Location.java
+++ src/main/java/pl/sda/hibernate/entity/Location.java
@@ -1,18 +1,47 @@
 package pl.sda.hibernate.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @Basic(optional = false)
     Integer longitude;
+    @Basic(optional = false)
     Integer latitude;
     String region;
+    @Basic(optional = false)
     String city;
+    @Basic(optional = false)
     String country;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return Objects.equals(id, location.id) && Objects.equals(longitude, location.longitude) && Objects
+                .equals(latitude, location.latitude) && Objects.equals(region, location.region) && Objects
+                .equals(city, location.city) && Objects.equals(country, location.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, longitude, latitude, region, city, country);
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                ", region='" + region + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }
